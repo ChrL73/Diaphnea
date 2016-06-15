2548e7e8f2224dc9fc29f22cf2e92c1079ea1bda
christophe <christoph92@yahoo.fr>
2016-06-15 16:22:03 +0200

    MongoDB experiments: data structure (continuation)

--- Experiments/NodeJS/MongoDbTest3_C_Find.js
+++ Experiments/NodeJS/MongoDbTest3_C_Find.js
@@ -3,15 +3,24 @@ var aSync = require("async");
 
 var db = mongoose.connect("mongodb://localhost/testDb");
 
+var n1 = 10;
+
 var questionSchema, categorySchema, testSchema;
 var TestModel;
 
-function testPerf(field)
+testPerf();
+
+function testPerf()
 {
    questionSchema = mongoose.Schema({ index: Number, text: String });
    categorySchema = mongoose.Schema({ index: Number, name: String, questions: [questionSchema] });
    levelSchema = mongoose.Schema({ name: String, categories: [categorySchema] }, { collection: "levels" + n1.toString() });
    LevelModel = mongoose.model("Level" + n1.toString(), levelSchema);
    
-   
+   LevelModel.findOne({ name: "Level1" }, { categories: { $elemMatch: { index: 2 } } }, function(err, level)
+   {
+      console.log(level);
+      //console.log(level.categories[0]);
+      //console.log(level.categories[0].questions[5].text);
+   });
 }

70fb80e2f879b47134a6c18400df8a0059c09b9a
christophe <christoph92@yahoo.fr>
2016-06-16 22:27:39 +0200

    MongoDB experiments: data structure (continuation)

--- Experiments/NodeJS/DropTestCollections3.js
+++ Experiments/NodeJS/DropTestCollections3.js
@@ -1,4 +1,4 @@
-/*db = connect("testDb");
+db = connect("testDb");
 
 db.levels10.drop();
 db.levels20.drop();
@@ -9,4 +9,15 @@ db.levels160.drop();
 db.levels320.drop();
 db.levels640.drop();
 db.levels1280.drop();
-db.levels2560.drop();*/
+db.levels2560.drop();
+
+db.questionLists10.drop();
+db.questionLists20.drop();
+db.questionLists30.drop();
+db.questionLists40.drop();
+db.questionLists.drop();
+db.questionLists.drop();
+db.questionLists320.drop();
+db.questionLists640.drop();
+db.questionLists1280.drop();
+db.questionLists2560.drop();

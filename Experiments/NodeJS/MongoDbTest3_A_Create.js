f6f4a480de2576f5fec95c3fcc22cf91498a5756
christophe <christoph92@yahoo.fr>
2016-06-16 22:27:39 +0200

    MongoDB experiments: data structure (continuation)

--- Experiments/NodeJS/MongoDbTest3_A_Create.js
+++ Experiments/NodeJS/MongoDbTest3_A_Create.js
@@ -123,55 +123,7 @@ function fillLevels(err)
          }
          else
          {
-            console.log("End");
-         }
-      });
-   }
-}
-
-/*function fillArray(err)
-{
-   if (err)
-   {
-      console.log(err);
-   }
-   else
-   {
-      console.log("n=" + n1 + " fillArray");
-
-      var iArray = getIndexArray(3);
-      
-      aSync.each(iArray, function(i, callback)
-      {
-         var level = new LevelModel();
-         level.name = "Level" + i.toString();
-
-         var j;
-         for (j = 0; j < 6; ++j)
-         {
-            var category = { index: j, name: "Category" + j.toString(), questions: [] };
-            
-            var k;
-            for (k = 0; k < (j + 1) * n1; ++k)
-            {
-               var question = { index: k, text: "Question" + k.toString() };
-               category.questions.push(question);
-            }
-            
-            level.categories.push(category);
-         }
-
-         level.save(function() { callback(false); });
-      },
-      function(err)
-      {
-         if (err)
-         {
-            console.log("err = " + err);
-         }
-         else
-         {
-            if (n1 < 10);//1500)
+            if (n1 < 1500)
             {
                n1 *= 2;
                clearQuestionsLists();
@@ -184,13 +136,6 @@ function fillLevels(err)
       });
    }
 }
-
-function getIndexArray(n)
-{
-   var i, array = [];
-   for (i = 0; i < n; ++i) array.push(i);
-   return array;
-}*/
 
 function getIndex2Array(n, m)
 {
--- Experiments/NodeJS/MongoDbTest3_C_Find.js
+++ Experiments/NodeJS/MongoDbTest3_C_Find.js
@@ -5,22 +5,49 @@ var db = mongoose.connect("mongodb://localhost/testDb");
 
 var n1 = 10;
 
-var questionSchema, categorySchema, testSchema;
+var questionSchema = mongoose.Schema({ index: Number, text: String });
+var questionListSchema;
+var questionListModel;
+
+var categorySchema = mongoose.Schema({ index: Number, name: String, questionList: mongoose.Schema.Types.ObjectId });
+var testSchema;
 var TestModel;
 
 testPerf();
 
 function testPerf()
 {
-   questionSchema = mongoose.Schema({ index: Number, text: String });
-   categorySchema = mongoose.Schema({ index: Number, name: String, questions: [questionSchema] });
+   questionListSchema = mongoose.Schema({ questions: [questionSchema] }, { collection: "questionLists" + n1.toString() });
+   QuestionListModel = mongoose.model("QuestionList" + n1.toString(), questionListSchema);
+   
    levelSchema = mongoose.Schema({ name: String, categories: [categorySchema] }, { collection: "levels" + n1.toString() });
    LevelModel = mongoose.model("Level" + n1.toString(), levelSchema);
    
-   LevelModel.findOne({ name: "Level1" }, { categories: { $elemMatch: { index: 2 } } }, function(err, level)
+   var i = 1, j = 2, k = 5;
+   
+   //LevelModel.findOne({ name: "Level" + i.toString() }, { categories: { $elemMatch: { index: j } } }, function(err, level)
+   LevelModel.findOne({ name: "Level" + i.toString() }, { categories: { $slice: [j, 1] } }, function(err, level)
    {
-      console.log(level);
-      //console.log(level.categories[0]);
-      //console.log(level.categories[0].questions[5].text);
+      if (err || !level)
+      {
+         console.log("err: " + err + " level: " + level);
+      }
+      else
+      {
+         console.log(level);
+         var questionListId = level.categories[0].questionList;
+
+         QuestionListModel.findOne({ _id: questionListId }, { questions: { $slice: [k, 1] } }, function(err, questionList)
+         {
+            if (err || !questionList)
+            {
+               console.log("err: " + err + " questionList: " + questionList);
+            }
+            else
+            {
+               console.log(questionList);
+            }
+         });
+      }
    });
 }

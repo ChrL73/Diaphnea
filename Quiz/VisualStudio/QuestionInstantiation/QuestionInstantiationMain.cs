34f1a8f5d707f3386394c428d098df57c246f33f
Christophe <christoph92@yahoo.fr>
2016-06-27 22:46:27 +0200

    Question instantiation: management of several questionnaires

--- Quiz/VisualStudio/QuestionInstantiation/QuestionInstantiationMain.cs
+++ Quiz/VisualStudio/QuestionInstantiation/QuestionInstantiationMain.cs
@@ -1,119 +1,33 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
+
 
 namespace QuestionInstantiation
 {
     class QuestionInstantiationMain
     {
-        private static QuizData _quizData;
-        private static List<Level> _levelList = new List<Level>();
-
         static void Main(string[] args)
         {
-            int result = 0;
+            if (args.Length < 1)
+            {
+                Console.WriteLine("usage: QuestionInstantiation path1 [path2 ...]");
+                return;
+            }
 
-            if (result == 0) result = loadData(args);
-            if (result == 0) result = instantiateQuestions();
-            if (result == 0) result = fillDatabase();
-
-            if (result == 0) Console.WriteLine("Question instantitation terminated successfully");
-            else Console.WriteLine("Question instantitation terminated with errors");
+            foreach (string path in args)
+            {
+                Questionnaire questionnaire = new Questionnaire();
+                questionnaire.instantiate(path);
+            }
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
 
-        static int loadData(string[] args)
-        {
-            if (args.Length < 1)
-            {
-                Console.WriteLine("No configuration file provided");
-                return -1;
-            }
-
-            XmlReader reader;
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.ValidationType = ValidationType.Schema;
-            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
-            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
-            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessIdentityConstraints;
-
-            try
-            {
-                reader = XmlReader.Create(args[0], settings);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Fail to open file {0}", args[0]);
-                Console.WriteLine(e.Message);
-                return -1;
-            }
-
-            Console.WriteLine("Reading configuration file " + args[0] + " ...");
-
-            XmlSerializer serializer = new XmlSerializer(typeof(XmlQuizData));
-            XmlQuizData quizData = null;
-
-            try
-            {
-                quizData = (XmlQuizData)serializer.Deserialize(reader);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Fail to deserialize content of file {0}", args[0]);
-                Console.WriteLine(e.Message);
-                if (e.InnerException != null) Console.WriteLine(e.InnerException.Message);
-                return -1;
-            }
-
-            reader.Close();
-
-            _quizData =  new QuizData(args[0], quizData);
-
-            MessageLogger.LogFileName = quizData.parameters.logFileName;
-            MessageLogger.FileLogLevel = quizData.parameters.fileLogLevel;
-            MessageLogger.ConsoleLogLevel = quizData.parameters.consoleLogLevel;
-
-            return 0;
-        }
-
-        static int instantiateQuestions()
-        {
-            foreach(XmlLevel xmlLevel in _quizData.XmlQuizData.parameters.levelList)
-            {
-                Level level = new Level();
-                if (level.initialize(_quizData, xmlLevel) != 0) return -1;
-                _levelList.Add(level);
-            }
-
-            return 0;
-        }
-
-        static int fillDatabase()
-        {
-            MongoClient mongoClient = new MongoClient();
-            IMongoDatabase database = mongoClient.GetDatabase(_quizData.XmlQuizData.parameters.databaseName);
-
-            BsonDocument filter = new BsonDocument();
-            IMongoCollection<BsonDocument> levelCollection = database.GetCollection<BsonDocument>("levels");
-            levelCollection.DeleteMany(filter);
-            IMongoCollection<BsonDocument> questionListsCollection = database.GetCollection<BsonDocument>("question_lists");
-            questionListsCollection.DeleteMany(filter);
-
-            foreach (Level level in _levelList)
-            {
-                if (level.fillDataBase(database) != 0) return -1;
-            }
-
-            return 0;
-        }
+        
     }
 }

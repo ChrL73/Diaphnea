1c4a4125d6d12b18297a7250252b3e44e93ef5f9
christophe <christoph92@yahoo.fr>
2016-06-30 14:54:35 +0200

    Quiz server: Some experiments with NodeJS, socket.io, html, jQuery...

--- Quiz/NodeJS/import_database.sh
+++ Quiz/NodeJS/import_database.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mongoimport --db diaphnea --drop --collection questionnaires --file ../../Shared/questionnaires.json
 mongoimport --db diaphnea --drop --collection levels --file ../../Shared/levels.json
 mongoimport --db diaphnea --drop --collection question_lists --file ../../Shared/question_lists.json

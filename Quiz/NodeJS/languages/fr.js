771abec0ccf958a85566712eb318fb56d86c2630
christophe <christoph92@yahoo.fr>
2016-07-06 16:37:50 +0200

    Quiz server: i18n initiation + home page continuation

---
+++ Quiz/NodeJS/languages/fr.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+   "questionnaire": "Questionnaire",
+   "language": "Langue",
+   "level": "Niveau",
+   "signUp" : "Inscription",
+   "signIn" : "Connexion",
+   "signOut" : "Déconnexion",
+   'name': 'Nom',
+   'password': 'Mot de passe'
+};

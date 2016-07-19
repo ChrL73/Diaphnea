db93f9063518ab5114ff4f8693bbe1c4ed8af830
christophe <christoph92@yahoo.fr>
2016-07-19 15:15:35 +0200

    Quiz server: Question production initiation

--- Quiz/NodeJS/public/game.js
+++ Quiz/NodeJS/public/game.js
@@ -1,16 +1,4 @@
 $(function()
 {
-   $('#siteLanguageSelect').change(function()
-   {
-      var languageId = $(this).val();
-      if (!userLogged)
-      {
-         document.cookie = 'siteLanguageId=' + languageId + getCookieExpires(180);
-         location.replace('/');
-      }
-      else
-      {
-         $('#siteLanguageForm').trigger('submit');
-      }
-   });
+   
 });

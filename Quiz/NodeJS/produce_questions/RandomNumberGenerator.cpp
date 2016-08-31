9693518882d04b4e3ca191fd766f94dd7ccdda60
christophe <christoph92@yahoo.fr>
2016-08-31 18:00:07 +0200

    C++ question producer: Random generator improvement

--- Quiz/NodeJS/produce_questions/RandomNumberGenerator.cpp
+++ Quiz/NodeJS/produce_questions/RandomNumberGenerator.cpp
@@ -3,35 +3,13 @@
 #include <cstdlib>
 #include <random>
 
-#if RAND_MAX != 0x7FFFFFFF
-    #ifdef _WIN32
-        #if RAND_MAX != 0x7FFF  
-            #error unexpected RAND_MAX value
-        #else
-            #pragma message("RandomNumberGenerator target is Linux. It will not behave as expected on Windows. Windows is used for development purpose")
-        #endif
-    #else
-        #error unexpected RAND_MAX value
-    #endif
-#endif
-
-#if CLOCKS_PER_SEC != 1000000
-    #ifdef _WIN32
-        #if CLOCKS_PER_SEC != 1000
-            #error unexpected RAND_MAX value
-        #else
-            #pragma message("RandomNumberGenerator target is Linux. It will not behave as expected on Windows. Windows is used for development purpose")
-        #endif
-    #else
-        #error unexpected CLOCKS_PER_SEC value
-    #endif
-#endif
-
 namespace produce_questions
 {
     bool RandomNumberGenerator::_initialized = false;
-    unsigned int RandomNumberGenerator::_shift[32];
-    int RandomNumberGenerator::_counter = 0;
+    unsigned int RandomNumberGenerator::_shift1[32];
+    unsigned int RandomNumberGenerator::_shift2[32];
+    int RandomNumberGenerator::_counter1 = 0;
+    int RandomNumberGenerator::_counter2 = 0;
 
     void RandomNumberGenerator::initialize(void)
     {
@@ -42,8 +20,28 @@ namespace produce_questions
         int i;
         for (i = 0; i < 32; ++i)
         {
-            _shift[i] = n;
+            _shift1[i] = n;
             n = (n << 1) | (n >> 31);
+        }
+
+        int *p = new int;
+        int q = 0;
+
+        #if defined(__linux)
+            long q0 = reinterpret_cast<long>(p);
+            int q1 = static_cast<int>((q0 & 0xffffffff00000000) >> 32);
+            int q2 = static_cast<int>(q0 & 0xffffffff);
+            q = q1 + q2;
+        #elif defined(_WIN32)
+            q = reinterpret_cast<int>(p);
+        #endif
+
+        delete p;
+
+        for (i = 0; i < 32; ++i)
+        {
+            _shift2[i] = q;
+            q = (q << 1) | (q >> 31);
         }
 
         _initialized = true;
@@ -52,22 +50,44 @@ namespace produce_questions
     int RandomNumberGenerator::verify(void)
     {
         if (sizeof(unsigned int) != 4) return -1;
+
         if (std::random_device::min() != 0 || std::random_device::max() != 0xFFFFFFFF) return -1;
+
+        #if defined(__linux) && !defined(_WIN32)
+            if (sizeof(int *) != 8) return -1;
+            if (sizeof(long) != 8) return -1;
+            if (RAND_MAX != 0x7FFFFFFF) return -1;
+            if (CLOCKS_PER_SEC != 1000000) return -1;
+        #elif defined(_WIN32)
+            if (sizeof(int *) != 4) return -1;
+            if (RAND_MAX != 0x7FFF) return -1;
+            if (CLOCKS_PER_SEC != 1000) return -1;
+        #else
+            #error Unknown platform
+        #endif
+
         if (!_initialized) initialize();
         return 0;
     }
 
-    unsigned int RandomNumberGenerator::shift(void)
+    unsigned int RandomNumberGenerator::shift1(void)
     {
-        ++_counter;
-        if (_counter == 32) _counter = 0;
-        return _shift[_counter];
+        ++_counter1;
+        if (_counter1 == 32) _counter1 = 0;
+        return _shift1[_counter1];
+    }
+
+    unsigned int RandomNumberGenerator::shift2(void)
+    {
+        ++_counter2;
+        if (_counter2 == 32) _counter2 = 0;
+        return _shift2[_counter2];
     }
 
     unsigned int RandomNumberGenerator::getRandomInt(void)
     {
         if (!_initialized) initialize();
-        return static_cast<unsigned int>(rand()) + shift() + static_cast<unsigned int>(std::clock());
+        return static_cast<unsigned int>(rand()) + shift1() + shift2() + static_cast<unsigned int>(std::clock());
     }
 
     double RandomNumberGenerator::getRandomDouble(void)
--- Quiz/NodeJS/produce_questions/RandomNumberGenerator.h
+++ Quiz/NodeJS/produce_questions/RandomNumberGenerator.h
@@ -9,10 +9,13 @@ namespace produce_questions
     private:
         static bool _initialized;
         static void initialize(void);
-        static unsigned int _shift[32];
-        static int _counter;
+        static unsigned int _shift1[32];
+        static unsigned int _shift2[32];
+        static int _counter1;
+        static int _counter2;
 
-        static unsigned int shift(void);
+        static unsigned int shift1(void);
+        static unsigned int shift2(void);
         static unsigned int getRandomInt(void);
         static double getRandomDouble (void);
         static double inverseCumulativeDistribution (double distribParameter, double y);

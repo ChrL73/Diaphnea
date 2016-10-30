cab0617425e187a13a01c54c6ada282658ad9d9c
christophe <christoph92@yahoo.fr>
2016-10-30 22:54:01 +0100

    NodeJS map server: Use 'readline' module to get C++ server answers

--- Map/NodeJS/cpp_server_interface.js
+++ Map/NodeJS/cpp_server_interface.js
@@ -1,4 +1,5 @@
 var childProcess = require('child_process');
+var readLine = require('readline');
 
 var cppProcess;
 var sendResponse;
@@ -20,28 +21,21 @@ function sendRequest(request, recursiveCall)
    {
       cppProcess = childProcess.spawn('./map_server.exe');
       
-      cppProcess.stdout.on('data', function(data)
+      readLine.createInterface(cppProcess.stdout, cppProcess.stdin).on('line', function(response)
       {
-         var dataArray = data.toString().split('\n');  
-         var responseArray = [];
-         dataArray.forEach(function(s) { if (s.length > 0) responseArray.push(s); });
-               
-         responseArray.forEach(function(response)
-         {
-            console.log('Response: ' + response);
-            
-            var i = response.indexOf(' ');
-            var socketId = response.substring(0, i);
-            response = response.substring(i + 1);
-            i = response.indexOf(' ');
-            var requestId = Number(response.substring(0, i));
-            response = response.substring(i + 1);
-            i = response.indexOf(' ');
-            var requestType = response.substring(0, i);
-            var responseContent = JSON.parse(response.substring(i + 1));
-            
-            sendResponse(socketId, requestId, requestType, responseContent);
-         });
+         console.log('Response: ' + response);
+
+         var i = response.indexOf(' ');
+         var socketId = response.substring(0, i);
+         response = response.substring(i + 1);
+         i = response.indexOf(' ');
+         var requestId = Number(response.substring(0, i));
+         response = response.substring(i + 1);
+         i = response.indexOf(' ');
+         var requestType = response.substring(0, i);
+         var responseContent = JSON.parse(response.substring(i + 1));
+         
+         sendResponse(socketId, requestId, requestType, responseContent);
       });
       
       if (recursiveCall) throw new Error(err); // Todo: hande error

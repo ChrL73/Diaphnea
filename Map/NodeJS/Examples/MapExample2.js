d63a69610e0883ebf452786170509868f8b39d60
Christophe <christoph92@yahoo.fr>
2016-12-19 22:25:29 +0100

    Add KML data for Mougoutsi and Basse-Banio departments

--- Map/NodeJS/Examples/MapExample2.js
+++ Map/NodeJS/Examples/MapExample2.js
@@ -6,7 +6,7 @@ $(function()
    function onConnected(mapServerConnection)
    {
       var canvasId = 'canvas';
-      var mapId = '_France';   
+      var mapId = '_Gabon';   
       mapServerConnection.loadMap(mapId, canvasId, onMapLoaded);
       
       function onMapLoaded(map)

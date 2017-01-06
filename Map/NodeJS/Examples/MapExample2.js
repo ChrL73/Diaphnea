d03a79bfafb9b183f7a046be4fd02236ba14acf8
Christophe <christoph92@yahoo.fr>
2017-01-06 22:40:15 +0100

    Add KML data for Gabon

--- Map/NodeJS/Examples/MapExample2.js
+++ Map/NodeJS/Examples/MapExample2.js
@@ -1,12 +1,12 @@
 $(function()
 {            
-   var url = 'http://192.168.50.194:3001';
+   var url = 'http://albertine:3001';
    mapServerInterface.createNewConnection(url, onConnected);
    
    function onConnected(mapServerConnection)
    {
       var canvasId = 'canvas';
-      var mapId = '_Test';   
+      var mapId = '_Gabon';   
       mapServerConnection.loadMap(mapId, canvasId, onMapLoaded);
       
       function onMapLoaded(map)

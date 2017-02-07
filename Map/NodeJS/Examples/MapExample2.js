613eb1ebe332882cf67ad0065e9299ea34f671e9
Christophe <christoph92@yahoo.fr>
2017-02-07 21:47:52 +0100

    Add KML Data: Ogoulou and Liboumba rivers

--- Map/NodeJS/Examples/MapExample2.js
+++ Map/NodeJS/Examples/MapExample2.js
@@ -3,13 +3,13 @@ $(function()
    document.addEventListener('wheel', function(e) { e.preventDefault(); });
    $('#svg').hide();
    
-   var url = 'http://192.168.50.61:3001';
+   var url = 'http://albertine:3001';
    mapServerInterface.createNewConnection(url, onConnected);
    
    function onConnected(mapServerConnection)
    {
       var canvasId = 'canvas';
-      var mapId = '_Test';   
+      var mapId = '_Gabon';   
       mapServerConnection.loadMap(mapId, canvasId, onMapLoaded);
       
       function onMapLoaded(map)

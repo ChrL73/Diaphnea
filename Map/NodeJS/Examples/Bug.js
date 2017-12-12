d56a438795949a06792fc9226367448fcd27b4c4
Christophe <christoph92@yahoo.fr>
2017-12-12 22:15:16 +0100

    Map front-end API: Bug correction (end)

--- Map/NodeJS/Examples/Bug.js
+++ Map/NodeJS/Examples/Bug.js
@@ -1,6 +1,6 @@
 $(function()
 {
-   var url = 'http://192.168.50.73:3001';
+   var url = 'http://albertine:3001';
    mapServerInterface.createNewConnection(url, onConnected);
    
    function onConnected(mapServerConnection)
@@ -25,12 +25,8 @@ $(function()
          });
          
          // Different behaviours for t=100 and t=200 (but bugs in the 2 cases)
-         //var t = 100;
+         var t = 100;
          //var t = 200;
-         
-         // 'popCounter' correction corrects the bug for t >~ 110
-         // but not for 100 <= t <~110
-         var t = 105;
          
          setTimeout(function()
          {
@@ -47,3 +43,5 @@ $(function()
    }
    
 });
+
+// The bug has been fixed thanks to the 'popCounter' variable in 'diaphnea_map_api.js'

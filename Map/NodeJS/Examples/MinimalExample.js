d1899fa65fe9c00db4ffe69240666fd5af409a32
Christophe <christoph92@yahoo.fr>
2018-03-11 18:34:59 +0100

    Add 'serverAddress.sh'

--- Map/NodeJS/Examples/MinimalExample.js
+++ Map/NodeJS/Examples/MinimalExample.js
@@ -1,6 +1,6 @@
 $(function()
 {
-   var url = 'http://192.168.50.61:3001';
+   var url = 'http://albertine:3001';
    mapServerInterface.createNewConnection(url, onConnected);
    
    function onConnected(mapServerConnection)
--- Quiz/React/quiz/src/index.js
+++ Quiz/React/quiz/src/index.js
@@ -13,7 +13,7 @@ class UserInterface extends React.Component
    {
       super(props);
       
-      this.socket = window.io.connect('192.168.50.184:3002');
+      this.socket = window.io.connect('albertine:3002');
       this.socket.on('displayPage', (data) => this.setState({ page: data.page }));
       
       this.state = { page: 'none' };

b230e2c4c7c107d8d6343c67e8d85d2c0ee391b5
Christophe <christoph92@yahoo.fr>
2018-03-20 22:12:15 +0100

    C++ map server: Ability to compile without png++ lib

--- Map/NodeJS/map_server/TextDisplayer/PngImage.cpp
+++ Map/NodeJS/map_server/TextDisplayer/PngImage.cpp
@@ -1,25 +1,36 @@
 #include "PngImage.h"
+
+#ifndef BUILD_WITHOUT_PNGPP
 #include <png++/png.hpp>
+#endif
 
 namespace map_server
 {
     PngImage::PngImage(int width, int height)
     {
+        #ifndef BUILD_WITHOUT_PNGPP
         _image = new png::image<png::rgb_pixel, png::pixel_buffer<png::rgb_pixel> >(width, height);
+        #endif
     }
 
     PngImage::~PngImage()
     {
+        #ifndef BUILD_WITHOUT_PNGPP
         delete _image;
+        #endif
     }
 
     void PngImage::setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
     {
+        #ifndef BUILD_WITHOUT_PNGPP
         (*_image)[y][x] = png::rgb_pixel(r, g, b);
+        #endif
     }
 
     void PngImage::save(std::string fileName)
     {
+        #ifndef BUILD_WITHOUT_PNGPP
         _image->write(fileName);
+        #endif
     }
 }

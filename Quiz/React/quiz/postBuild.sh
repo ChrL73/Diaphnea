e860fe2867ec7fbc668efde001a335198b62071c
christophe <christoph92@yahoo.fr>
2018-03-25 09:39:19 +0200

    Quiz server: React front-end: postBuild.sh: Correction

--- Quiz/React/quiz/postBuild.sh
+++ Quiz/React/quiz/postBuild.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -v
+set -e
+mkdir -p ../../NodeJS/views
 cp build/index.html ../../NodeJS/views/reactIndex.ejs
 sed -i -E "s/http:\/\/.+(\/socket.io\/socket.io.js)/\1/" ../../NodeJS/views/reactIndex.ejs
 rm -rf ../../NodeJS/public/static

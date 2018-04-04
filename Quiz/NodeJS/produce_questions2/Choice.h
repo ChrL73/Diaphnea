439be0fd564ffacd9d639325e88a22dc20bbb21e
christophe <christoph92@yahoo.fr>
2018-04-04 23:09:26 +0200

    Add comments

--- Quiz/NodeJS/produce_questions2/Choice.h
+++ Quiz/NodeJS/produce_questions2/Choice.h
@@ -7,14 +7,25 @@ namespace produce_questions
     class Choice
     {
     private:
+        // String
         int _choiceText;
+
+        // String
         int _comment;
+
+        // String
         int _mapId;
+
+        // Array of doubles
         int _doubleCriterionValueCount;
         int _doubleCriterionValues;
+
+        // Array of strings
         int _stringCriterionValueCount;
         int _stringCriterionValues;
-        int _pointCriterionValueCount;
+
+        // Array of 3*n doubles (X1, Y1, Z1, X2, Y2, Z2, ... Xn, Yn, Zn)
+        int _pointCriterionValueCount; // = n
         int _pointCriterionValues;
 
     public:

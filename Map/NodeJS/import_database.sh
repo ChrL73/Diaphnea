b65f7671cdfaeb272b70cfde77ef7bc1264cca89
Christophe <christoph92@yahoo.fr>
2018-07-06 21:47:39 +0200

    Remove old files

--- Map/NodeJS/import_database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mongoimport --db diaphnea --drop --collection maps --file ../../Shared/maps.json
-mongoimport --db diaphnea --drop --collection polygon_elements --file ../../Shared/polygon_elements.json
-mongoimport --db diaphnea --drop --collection point_elements --file ../../Shared/point_elements.json
-mongoimport --db diaphnea --drop --collection line_elements --file ../../Shared/line_elements.json
-mongoimport --db diaphnea --drop --collection items --file ../../Shared/items.json
-mongoimport --db diaphnea --drop --collection point_lists --file ../../Shared/point_lists.json
--- Quiz/VisualStudio/QuestionInstantiation/ExportDatabase.bat
+++
@@ -1,4 +0,0 @@
-mongoexport --db diaphnea --collection questionnaires --out ../../../Shared/questionnaires.json
-mongoexport --db diaphnea --collection levels --out ../../../Shared/levels.json
-mongoexport --db diaphnea --collection choice_lists --out ../../../Shared/choice_lists.json
-mongoexport --db diaphnea --collection question_lists --out ../../../Shared/question_lists.json
